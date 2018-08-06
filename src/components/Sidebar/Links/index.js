25c3e2900014f74e202fbf210141c1c156a18e62
dahby <dstoll243@gmail.com>
2018-08-06 15:33:59 -0700

    updated social links

--- src/components/Sidebar/Links/index.js
+++ src/components/Sidebar/Links/index.js
@@ -9,28 +9,28 @@ class Links extends Component {
       <div className="links">
         <ul className="icons-list">
           <li className="icon">
-            <a href="https://www.github.com/praagyajoshi" target="_blank">
+            <a href="https://www.github.com/dahby" target="_blank">
               <i className="fa fa-github"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="https://www.twitter.com/praagyajoshi" target="_blank">
+            <a href="https://www.twitter.com/davidcstoll" target="_blank">
               <i className="fa fa-twitter"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="https://www.linkedin.com/in/praagyajoshi" target="_blank">
+            <a href="https://www.linkedin.com/in/dstoll243" target="_blank">
               <i className="fa fa-linkedin"></i>
             </a>
           </li>
           <li className="icon">
-            <a href="mailto:hello@praagya.com" target="_blank">
+            <a href="mailto:dstoll243@gmail.com" target="_blank">
               <i className="fa fa-envelope"></i>
             </a>
           </li>
         </ul>
         <div className="small">
-          Built with ❤️ using <a href="https://www.gatsbyjs.org/" target="_blank">GatsbyJS</a>
+          Built using <a href="https://www.gatsbyjs.org/" target="_blank">GatsbyJS</a>
         </div>
       </div>
     )
