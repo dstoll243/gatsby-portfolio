e68d11933d9ff73dd32df9765bf38e92e04dd7ba
dahby <dstoll243@gmail.com>
2018-08-06 14:15:06 -0700

    init commit

---
+++ src/components/Sidebar/About/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+import './style.scss'
+
+class About extends Component {
+  render () {
+    return (
+      <div className="about">
+        <div className="image">
+        </div>
+        <div className="bio">
+          David Stoll is a Full-Stack JavaScript Developer (MERN) 
+          with background in retail banking. David is driven by a collaborative mindset and a desire to improve lives through technical skills.
+        </div>
+      </div>
+    )
+  }
+}
+
+export default About
